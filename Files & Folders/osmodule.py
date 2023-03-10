572f78125e86263f23933223208ae876378712eb
Chester991 <ayushbalwani991@gmail.com>
2023-03-10 16:55:13 +0530

    os module

--- Files & Folders/osmodule.py
+++ Files & Folders/osmodule.py
@@ -4,4 +4,4 @@ print(os.path.join('folder1','folder2','folder3'))
 print(os.sep) # prints the value that the join function uses
 print(os.getcwd()) # get current directory
 os.chdir('/home/ayush/Documents') #change current directory to the one passed inside funtion.
-print(os.getcwd())
+print(os.getcwd())# prints current working directory 
